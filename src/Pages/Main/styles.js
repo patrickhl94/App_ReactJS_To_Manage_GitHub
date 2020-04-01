59b2c2dde1b5e0eb117b8868a788947406741d44
Patrick Lima <patrickhl94@hotmail.com>
2020-03-31 21:06:22 -0300

    Created styles.js Main

--- src/Pages/Main/styles.js
+++ src/Pages/Main/styles.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #fff;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
 `;
+
+export const Form = styled.form``;
+
+export const SubmitButton = styled.button``;
