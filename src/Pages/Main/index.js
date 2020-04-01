913db062bf0e2d43aeae0ff78f6b7d9fbd90cc17
Patrick Lima <patrickhl94@hotmail.com>
2020-04-01 00:59:57 -0300

    Updated index.js Main

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adcionar Repositório" />
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton>
-          <FaPlus color="#eee" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo } = this.state;
+
+    const resp = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: resp.data.full_name,
+    };
+
+    this.setState({
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      repositories: [...this.state.repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adcionar Repositório"
+            value={this.state.newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={this.state.loading}>
+            {this.state.loading ? (
+              <FaSpinner color="#eee" size={14} />
+            ) : (
+              <FaPlus color="#eee" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
