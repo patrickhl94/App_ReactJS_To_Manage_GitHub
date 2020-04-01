5e4dfad6aafb705cf062724bdd8880134567281e
Patrick Lima <patrickhl94@hotmail.com>
2020-03-31 21:05:18 -0300

    Created index.js Main

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Wello Word!</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adcionar Repositório" />
+
+        <SubmitButton dissabled>
+          <FaPlus />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
