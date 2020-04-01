cc6a03b5d10fc272e6fe706eadcec3c348245a3d
Patrick Lima <patrickhl94@hotmail.com>
2020-03-31 21:05:49 -0300

    Created index.js Repository

--- src/Pages/Repository/index.js
+++ src/Pages/Repository/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { FaGithubAlt } from 'react-icons/fa';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Repository() {
-  return <h1>Repository</h1>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt /> Repositórios
+      </h1>
+    </Container>
+  );
 }
