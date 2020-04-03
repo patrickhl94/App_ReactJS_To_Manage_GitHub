e3434fc8a0214fc68bf13cd560464004ad5c87ad
Patrick Lima <patrickhl94@hotmail.com>
2020-04-02 22:41:52 -0300

    Updated index.js Repository

--- src/Pages/Repository/index.js
+++ src/Pages/Repository/index.js
@@ -7,19 +7,34 @@ import { Container } from './styles';
 
 export default class Repository extends Component {
   async componentDidMount() {
+    const state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
     const { match } = this.props;
 
     const repoName = decodeURIComponent(match.params.repository);
 
     const [reposirory, issues] = await Promise.all([
       api.get(`/repos/${repoName}`),
-      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
     ]);
-    console.log(reposirory.data);
-    console.log(issues.data);
+
+    this.setState({
+      reposirory: reposirory.data,
+      issues: issues.data,
+      loading: false,
+    });
   }
 
   render() {
+    const { loading, issues, repository } = this.state;
     return (
       <Container>
         <h1>Repo</h1>
