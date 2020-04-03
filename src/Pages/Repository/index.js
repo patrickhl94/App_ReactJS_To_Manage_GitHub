bf05b2e8ae3445c4c437bdc740a73c4b43f63355
Patrick Lima <patrickhl94@hotmail.com>
2020-04-02 22:19:36 -0300

    Updated index.js Repository

--- src/Pages/Repository/index.js
+++ src/Pages/Repository/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithubAlt } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container } from './styles';
 
-export default function Repository() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt /> Repositórios
-      </h1>
-    </Container>
-  );
+export default class Repository extends Component {
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [reposirory, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}`),
+    ]);
+    console.log(reposirory.data);
+    console.log(issues.data);
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>Repo</h1>
+      </Container>
+    );
+  }
 }
