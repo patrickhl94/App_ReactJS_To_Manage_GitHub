8199b0c71e5b3ce5918dbe739b7ceeb487efacdf
Patrick Lima <patrickhl94@hotmail.com>
2020-04-06 08:54:08 -0300

    Updated styles.js Repository

--- src/Pages/Main/styles.js
+++ src/Pages/Main/styles.js
@@ -11,6 +11,7 @@ export const Form = styled.form`
     padding: 10px 15px;
     border-radius: 4px;
     font-size: 16px;
+    border: 1px solid ${(props) => (props.borderColor ? '#eee' : 'red')};
   }
 `;
 
